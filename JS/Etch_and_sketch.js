0e8fbac60c34eb97838713331f192ffff9c316fa
Najwanlaskar <Najwanlaskar12@example.com>
2025-10-10 13:38:48 +0100

    Added Etch_and_Sketch data
    Addd Etch and sketch data from past project and also moved Etch_and_sketch.js file into JS

---
+++ JS/Etch_and_sketch.js
@@ -0,0 +1,56 @@
+//put the grind squares inside a container div (HTML)
+const Container=document.querySelector(".container");
+const btn=document.querySelector(".Display");
+const colbtn=document.querySelector(".color");
+
+let containerSize=520;
+let size=16;
+let shade= null
+let rainbow=false
+function CreateColumn(size){
+        Container.innerHTML = "";
+        for(i=0;i<size*size;i++){                                                    
+        const square = document.createElement("div");
+        square.classList.add("square")
+        square.style.border = "1px solid";
+          square.style.background="white"
+        square.style.height= square.style.width= `${(containerSize/size)-2}px`;
+        Container.appendChild(square);
+
+        square.addEventListener("mouseover", (e)=>{
+        
+            if (rainbow === true) {
+                let r = Math.floor(Math.random() * 256); 
+                let g = Math.floor(Math.random() * 256); 
+                let b = Math.floor(Math.random() * 256); 
+                square.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
+            } else {
+             square.style.background="black"}
+{
+} })
+}
+
+}
+
+btn.textContent="Display Size"
+btn.addEventListener("click",(e)=>{
+        size = prompt("What would you like the size to be?")
+        CreateColumn(size)
+
+});
+
+colbtn.textContent="Rainbow Mode"
+colbtn.addEventListener("click",(e)=>{
+if(rainbow)
+      {
+          rainbow = false;
+      }
+      else
+      {
+          rainbow = true;
+      }
+})
+
+
+CreateColumn(size)
+
